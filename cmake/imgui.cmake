71656a1833e994ccfa83e2af9164cb2f81a01ab8
Bobby Battista <bobtista@gmail.com>
2024-12-01 15:58:38 -0500

    build: Update CMake configuration for cross-platform ImGui support

--- cmake/imgui.cmake
+++ cmake/imgui.cmake
@@ -5,16 +5,27 @@ FetchContent_Populate(imgui DOWNLOAD_EXTRACT_TIMESTAMP
     SOURCE_DIR ${IMGUI_DIR}
 )
 
+# Common ImGui sources for all platforms
+set(IMGUI_SOURCES
+    ${IMGUI_DIR}/imgui.cpp
+    ${IMGUI_DIR}/imgui_draw.cpp
+    ${IMGUI_DIR}/imgui_demo.cpp
+    ${IMGUI_DIR}/imgui_tables.cpp
+    ${IMGUI_DIR}/imgui_widgets.cpp
+    ${IMGUI_DIR}/misc/cpp/imgui_stdlib.cpp
+)
+
+set(IMGUI_HEADERS
+    ${IMGUI_DIR}/imconfig.h
+    ${IMGUI_DIR}/imgui.h
+    ${IMGUI_DIR}/imgui_internal.h
+    ${IMGUI_DIR}/misc/cpp/imgui_stdlib.h
+)
+
 if(WINDOWS)
-    add_library(imgui_win32 STATIC
-        ${IMGUI_DIR}/imgui.cpp
-        ${IMGUI_DIR}/imgui_draw.cpp
-        ${IMGUI_DIR}/imgui_demo.cpp
-        ${IMGUI_DIR}/imgui_tables.cpp
-        ${IMGUI_DIR}/imgui_widgets.cpp
+    add_library(imgui_win32 STATIC ${IMGUI_SOURCES}
         ${IMGUI_DIR}/backends/imgui_impl_dx9.cpp
         ${IMGUI_DIR}/backends/imgui_impl_win32.cpp
-        ${IMGUI_DIR}/misc/cpp/imgui_stdlib.cpp
     )
 
     if (CMAKE_GENERATOR MATCHES "Visual Studio")
@@ -25,15 +36,46 @@ if(WINDOWS)
     endif()
 
     target_sources(imgui_win32 PRIVATE
-        ${IMGUI_DIR}/imconfig.h
-        ${IMGUI_DIR}/imgui.h
+        ${IMGUI_HEADERS}
         ${IMGUI_DIR}/backends/imgui_impl_dx9.h
         ${IMGUI_DIR}/backends/imgui_impl_win32.h
-        ${IMGUI_DIR}/misc/cpp/imgui_stdlib.h
     )
 
     target_include_directories(imgui_win32 PUBLIC
         ${IMGUI_DIR}
         ${IMGUI_DIR}/backends
     )
+    set(IMGUI_LIBRARY imgui_win32)
+else()
+    # Unix/Linux build with OpenGL backend
+    include(FindPkgConfig)
+    pkg_check_modules(GLFW REQUIRED glfw3)
+
+    add_library(imgui STATIC ${IMGUI_SOURCES}
+        ${IMGUI_DIR}/backends/imgui_impl_glfw.cpp
+        ${IMGUI_DIR}/backends/imgui_impl_opengl3.cpp
+    )
+
+    target_sources(imgui PRIVATE
+        ${IMGUI_HEADERS}
+        ${IMGUI_DIR}/backends/imgui_impl_glfw.h
+        ${IMGUI_DIR}/backends/imgui_impl_opengl3.h
+    )
+
+    target_include_directories(imgui PUBLIC
+        ${IMGUI_DIR}
+        ${IMGUI_DIR}/backends
+        ${GLFW_INCLUDE_DIRS}
+    )
+
+    target_link_libraries(imgui PUBLIC
+        GL
+        ${GLFW_LIBRARIES}
+    )
+
+    target_link_directories(imgui PUBLIC
+        ${GLFW_LIBRARY_DIRS}
+    )
+
+    set(IMGUI_LIBRARY imgui)
 endif()
